9949c6da1c72d6efde6aeb3cee8d2118bd6f2bed
gillchristian <gillchristiang@gmail.com>
2019-02-12 01:27:19 +0100

    Update theme and slides

--- theme.js
+++ theme.js
@@ -1,12 +1,31 @@
-import theme from 'mdx-deck/themes'
+import { book as theme } from "mdx-deck/themes";
+
+import style from "react-syntax-highlighter/styles/prism/twilight";
+
+import rust from "react-syntax-highlighter/languages/prism/rust";
+import go from "react-syntax-highlighter/languages/prism/go";
+import csharp from "react-syntax-highlighter/languages/prism/csharp";
+import scala from "react-syntax-highlighter/languages/prism/scala";
+import elm from "react-syntax-highlighter/languages/prism/elm";
+import haskell from "react-syntax-highlighter/languages/prism/haskell";
+import reason from "react-syntax-highlighter/languages/prism/reason";
+import css from "react-syntax-highlighter/languages/prism/css";
+import typescript from "react-syntax-highlighter/languages/prism/typescript";
 
 export default {
   ...theme,
-
-  // Customize your presentation theme here.
-  //
-  // Read the docs for more info:
-  // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
-  // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
-
-}
+  prism: {
+    style,
+    languages: {
+      rust,
+      go,
+      csharp,
+      scala,
+      elm,
+      haskell,
+      reason,
+      css,
+      typescript
+    }
+  }
+};
